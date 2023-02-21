5ba3689e1b033ca8377a397839f839bf29a054a5
zlordelo <joao.system1@gmail.com>
2023-02-21 08:59:29 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,50 +1,49 @@
-const music = document.querySelector('#music')
+const music = document.querySelector("#music");
+const startPage = document.querySelector("#startPage");
+const btnStart = document.querySelector("#startButton");
+const page1 = document.querySelector("#page1");
+const page2 = document.querySelector("#page2");
+
+btnStart.addEventListener("click", () => {
+  startPage.classList.add("hidden");
+  music.play()
+  page1.classList.remove("hidden");
+});
 
 let playPause = () => {
-    if (music.paused) {
-        music.play()
-    } else {
-        music.pause()
-    }
-}
+  if (music.paused) {
+    music.play();
+  } else {
+    music.pause();
+  }
+};
 
 let mute = () => {
-    if (!music.paused && !music.muted) {
-        music.muted = true
-    } 
-}
+  if (!music.paused && !music.muted) {
+    music.muted = true;
+  }
+};
 
 let unmute = () => {
-    if (!music.paused && music.muted) {
-        music.muted = false
-    } 
-}
+  if (!music.paused && music.muted) {
+    music.muted = false;
+  }
+};
 
-const darkMode = () => {
-    let darkModeColor = 'rgb(0, 0, 0)'
-    let backgroundColor = document.body.style.backgroundColor
-    if (backgroundColor != darkModeColor){
-        document.body.style.backgroundColor = "#000"
-    } else {
-        document.body.style.backgroundColor = "#F2D54B"
-    }
-    
-}
+const video = document.getElementById("video");
+const btnCamera = document.getElementById("camera");
 
-const video = document.getElementById('video');
-const playButton = document.getElementById('btnPlay');
-
-playButton.addEventListener('click', event => {
-    const constraints = {
-      video: true,
-      audio: false
-    };
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then(stream => {
-        video.srcObject = stream;
-      })
-      .catch(error => {
-        console.error(error);
-      });
+btnCamera.addEventListener("click", (event) => {
+  const constraints = {
+    video: true,
+    audio: false,
+  };
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then((stream) => {
+      video.srcObject = stream;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
